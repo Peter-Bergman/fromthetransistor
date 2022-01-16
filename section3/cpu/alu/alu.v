19c7c8483dba9eebeee109ab2b1cf1d353140478
Peter-Bergman <peterbergman8253@gmail.com>
2022-01-16 17:47:44 -0600

    I think I finished the logic for the ALU. I still need to add the wire declarations. What's cool is that there are no registers because the ALU has exclusively combinatorial logic.

--- section3/cpu/alu/alu.v
+++ section3/cpu/alu/alu.v
@@ -10,6 +10,7 @@ module alu(
 	branch_immediate,
 	upper_immediate,
 	jump_immediate,
+	shamt,
 
 	alu_result,
 	branch_result,
@@ -72,13 +73,91 @@ always_comb begin
 	else if (op_code == 7'b0100011) begin // SB SH SW
 		alu_result = reg1 + immediate;
 	end
-	else if (op_code == 7'b0010111) begin
+	else if (op_code == 7'b0010011) begin
 		case (func3)
 			3'b000 : begin // ADDI
 				alu_result = reg1 + immediate;
 			end
 			3'b010 : begin // SLTI
-				if (signed'(reg1) < signed'(immediate))
+				if (signed'(reg1) < signed'(immediate)) begin
+					alu_result = 32'b1;
+				end else begin
+					alu_result = 32'b0;
+				end
+			end
+			3'b011 : begin //SLTIU
+				if (reg1 < immediate) begin
+					alu_result = 32'b1;
+				end else begin
+					alu_result = 32'b0;
+				end
+			end
+			3'b100 : begin // XORI
+				alu_result = reg1 ^ immediate;
+			end
+			3'b110 : begin // ORI
+				alu_result = reg1 | immediate;
+			end
+			3'b111 : begin // ANDI
+				alu_result = reg1 & immediate;
+			end
+			3'b001 : begin // SLLI
+				alu_result = reg1 << shamt;
+			3'b101 : begin // SRLI SRAI
+				if (func7 == 7'b0) begin // SRLI
+					alu_result = reg1 >> shamt;
+				end else if (func7 == 7'b0100000) begin // SRAI
+					alu_result = reg1 >>> shamt;
+				end
+			end
+		endcase
+	end
+	else if (op_code == 7'b0110011) begin
+		case (func3)
+			3'b000 : begin // ADD SUB
+				if (func7 == 7'b0) begin // ADD
+					alu_result = reg1 + reg2;
+				end else if (func7 == 7'b0100000) begin // SUB
+					alu_result = reg1 - reg2;
+				end
+			end
+			3'b001 : begin // SLL
+				alu_result = reg1 << reg2[4:0];
+			end
+			3'b010 : begin // SLT
+				if (signed'(reg1) < signed'(reg2)) begin
+			    		alu_result = 32'b1;
+				end else begin
+					alu_result = 32'b0;
+				end
+			end
+			3'b011 : begin // SLTU
+				if (reg1 < reg2) begin
+					alu_result = 32'b1;
+				end else begin
+					alu_result = 32'b0;
+				end
+			end
+			3'b100 : begin // XOR
+				alu_result = reg1 ^ reg2;
+			end
+			3'b101 : begin // SRL SRA
+				if (func7 == 7'b0) begin
+					alu_result = reg1 >> reg2[4:0]; // SRL
+				end else if (func7 == 7'b0100000) begin
+					alu_result = reg1 >>> reg2[4:0]; // SRA
+				end
+			end
+			3'b110 : begin // OR
+				alu_result = reg1 | reg2;
+			end
+			3'b111 : begin // AND
+				alu_result = reg1 & reg2;
+			end
+		endcase
+	end
+
+					
 		
 
 end
