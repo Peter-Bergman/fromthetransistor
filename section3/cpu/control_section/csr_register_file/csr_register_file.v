0a9a1cf58a6fcd441ed796bae0ff76cb9f429915
Peter-Bergman <peterbergman8253@gmail.com>
2022-02-04 12:15:33 -0600

    added set and clear functionality to CSR register file

--- section3/cpu/control_section/csr_register_file/csr_register_file.v
+++ section3/cpu/control_section/csr_register_file/csr_register_file.v
@@ -33,6 +33,8 @@ module CSR_Register_File(
 	wire			has_read_permission,
 				has_write_permission;
 
+	integer i;
+
 	// logic
 	
 	// For now, we will just give the user permission to whatever they
@@ -81,8 +83,50 @@ module CSR_Register_File(
 
 		if (write_back == 2'b01) begin
 			// clear
+			if (write_back_permission) begin
+				for (i = 0; i < 32; i = i + 1) begin
+					case (write_back_address)
+						12'hC01 :
+							cycle[i] <= write_back_data[i] ? 1'b0 : cycle[i];
+						12'hC02 :
+							time_[i] <= write_back_data[i] ? 1'b0 : time_[i];
+						12'hC03 :
+							instret[i] <= write_back_data[i] ? 1'b0 : instret[i];
+						12'hC80 :
+							cycleh[i] <= write_back_data[i] ? 1'b0 : cycleh[i];
+						12'hC81 :
+							timeh[i] <= write_back_data[i] ? 1'b0 : timeh[i];
+						12'hC82 :
+							instreth[i] <= write_back_data[i] ? 1'b0 : instreth[i];
+						default :
+							// Invalid address
+							// trap
+					endcase
+				end
+			end
 		end else if (write_back == 2'b10) begin
 			// set
+			if (write_back_permission) begin
+				for (i = 0; i < 32; i = i + 1) begin
+					case (write_back_address)
+						12'hC01 :
+							cycle[i] <= write_back_data[i] ? 1'b1 : cycle[i];
+						12'hC02 :
+							time_[i] <= write_back_data[i] ? 1'b1 : time_[i];
+						12'hC03 :
+							instret[i] <= write_back_data[i] ? 1'b1 : instret[i];
+						12'hC80 :
+							cycleh[i] <= write_back_data[i] ? 1'b1 : cycleh[i];
+						12'hC81 :
+							timeh[i] <= write_back_data[i] ? 1'b1 : timeh[i];
+						12'hC82 :
+							instreth[i] <= write_back_data[i] ? 1'b1 : instreth[i];
+						default :
+							// Invalid address
+							// trap
+					endcase
+				end
+			end
 		end else if (write_back == 2'b11) begin
 			// write
 			if (write_back_permission) begin
@@ -100,7 +144,8 @@ module CSR_Register_File(
 						12'hC82 :
 							instreth <= write_back_data;
 						default :
-							// Invalid address trap
+							// Invalid address
+							// trap
 				endcase
 			end // end of the write_back_permission block
 
