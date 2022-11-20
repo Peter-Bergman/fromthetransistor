0663c2bf537d494b0498f8ddc4efa1d5bbecffc2
Peter Bergman <peterbergman8253@gmail.com>
2022-11-20 07:48:33 -0600

    Added parser for Undef directive in Preprocessor in its own module
    I had to refactor the non-AST parsing code in ControlLine module
    
    I will remove all the extra code in ControlLine once I am finished
    writing the parsers for each type of ControlLine in their own module.

---
+++ section4/ccompiler/Preprocessor/UndefDirective.hs
@@ -0,0 +1,29 @@
+module UndefDirective where
+import AbstractSyntaxTree
+    (ControlLine( UndefDirective ))
+import Identifier
+    (identifier)
+import NewLine
+    (newLine)
+import Octothorpe
+    (octothorpe)
+import PreprocessingParser
+    ( PreprocessingParserX
+    , stringSatisfy_
+    )
+import Text.Parsec.Combinator
+    (between)
+import Text.Parsec.Prim
+    ((<?>))
+
+undefDirective :: PreprocessingParserX ControlLine
+undefDirective = do
+    parsedIdentifier <- between undefPrefix newLine identifier
+    return $ UndefDirective parsedIdentifier
+
+undefPrefix :: PreprocessingParserX ()
+undefPrefix = octothorpe >> undef >> return ()
+
+undef :: PreprocessingParserX ()
+undef = stringSatisfy_ (=="undef")
+
