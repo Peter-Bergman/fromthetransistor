38e4e29958203549aa831d21915cfbdecb5927de
Peter Bergman <peterbergman8253@gmail.com>
2023-05-15 19:58:58 -0500

    Began refactor on Lexer.PreprocessingToken module to use AST types

--- section4/ccompiler/src/Lexer/PreprocessingToken.hs
+++ section4/ccompiler/src/Lexer/PreprocessingToken.hs
@@ -1,4 +1,4 @@
-module Lexer.PreprocessingToken 
+module Lexer.PreprocessingToken
 ( preprocessingToken
 , headerName
 , identifier
@@ -8,32 +8,52 @@ module Lexer.PreprocessingToken
 , punctuator
 , otherCharacter
 ) where
+import AbstractSyntaxTree
 import CharTokenParsers.Constants.CharacterConstants.CharacterConstant
 import CharTokenParsers.HeaderNames.HeaderName
 import CharTokenParsers.Identifiers.Identifier
 import CharTokenParsers.PPNumbers.PPNumber
 import CharTokenParsers.Punctuators.Punctuator
 import CharTokenParsers.StringLiterals.StringLiteral
+import CustomCombinators
 import Data.Char
 import Text.Parsec
 import Text.Parsec.String
 
 
-preprocessingToken :: Parser String
-preprocessingToken = 
-    try (headerName) <|>
-    try (characterConstant) <|>
-    try (identifier) <|>
-    try (ppNumber) <|>
-    try (stringLiteral) <|>
-    try (punctuator) <|>
-    otherCharacter
+preprocessingToken :: Parser PreprocessingToken
+preprocessingToken =
+    try (headerNamePreprocessingToken) <|>
+    try (characterConstantPreprocessingToken) <|>
+    try (identifierPreprocessingToken) <|>
+    try (ppNumberPreprocessingToken) <|>
+    try (stringLiteralPreprocessingToken) <|>
+    try (punctuatorPreprocessingToken) <|>
+    otherCharacterPreprocessingToken
 
-otherCharacter :: Parser String
-otherCharacter = do
+headerNamePreprocessingToken :: Parser PreprocessingToken
+headerNamePreprocessingToken = simpleExpression headerName HeaderNamePreprocessingToken
+
+characterConstantPreprocessingToken :: Parser PreprocessingToken
+characterConstantPreprocessingToken = simpleExpression characterConstant CharacterConstantPreprocessingToken
+
+identifierPreprocessingToken :: Parser PreprocessingToken
+identifierPreprocessingToken = simpleExpression identifier IdentifierPreprocessingToken
+
+ppNumberPreprocessingToken :: Parser PreprocessingToken
+ppNumberPreprocessingToken = simpleExpression ppNumber PPNumberPreprocessingToken
+
+stringLiteralPreprocessingToken :: Parser PreprocessingToken
+stringLiteralPreprocessingToken = simpleExpression stringLiteral StringLiteralPreprocessingToken
+
+punctuatorPreprocessingToken :: Parser PreprocessingToken
+punctuatorPreprocessingToken = simpleExpression punctuator PunctuatorPreprocessingToken
+
+otherCharacterPreprocessingToken :: Parser PreprocessingToken
+otherCharacterPreprocessingToken = do
     parsedCharacter <- satisfy nonSpace
     return $ parsedCharacter : ""
-    
+
 nonSpace :: Char -> Bool
 nonSpace = not . isSpace
 
