ee1ffee21359e517b2191ca03a9c6c135ab84ea3
Peter Bergman <peterbergman8253@gmail.com>
2023-05-16 19:59:22 -0500

    Refactored CharTokenParsers.Punctuators.Punctuator module to use AST types

--- section4/ccompiler/src/CharTokenParsers/Punctuators/Punctuator.hs
+++ section4/ccompiler/src/CharTokenParsers/Punctuators/Punctuator.hs
@@ -1,66 +1,68 @@
 module CharTokenParsers.Punctuators.Punctuator where
+import AbstractSyntaxTree
+import CustomCombinators
 import Text.Parsec
 import Text.Parsec.Combinator
 import Text.Parsec.String
 
-punctuator :: Parser String
-punctuator = 
+punctuator :: Parser Punctuator
+punctuator = tryWithFailMessage "Punctuator" $ do
     {-
       The order here is different than in the ISO document.
       This is because of precedence.
       The "&" string will be parsed before "&&" which is not correct.
     -}
-    string "[" <|>
-    string "]" <|>
-    string "(" <|>
-    string ")" <|>
-    string "{" <|>
-    string "}" <|>
-    try (string "...") <|>
-    string "." <|>
-    try (string "->") <|>
-    try (string "++") <|>
-    try (string "--") <|>
-    try (string "*=") <|>
-    try (string "/=") <|>
-    try (string "%=") <|>
-    try (string "+=") <|>
-    try (string "-=") <|>
-    try (string "<<=") <|>
-    try (string ">>=") <|>
-    try (string "&=") <|>
-    try (string "^=") <|>
-    try (string "|=") <|>
-    try (string "<=") <|>
-    try (string ">=") <|>
-    try (string "==") <|>
-    try (string "!=") <|>
-    try (string "<:") <|>
-    try (string ":>") <|>
-    try (string "<%") <|>
-    try (string "%>") <|>
-    try (string "%:%:") <|>
-    try (string "%:") <|>
-    try (string "&&") <|>
-    try (string "||") <|>
-    string "&" <|>
-    string "*"<|>
-    string "+" <|>
-    string "-" <|>
-    string "~" <|>
-    string "!" <|>
-    string "/" <|>
-    string "%" <|>
-    try (string "<<") <|>
-    try (string ">>") <|>
-    string "<" <|>
-    string ">" <|>
-    string "^" <|>
-    string "|" <|>
-    string "?" <|>
-    string ":" <|>
-    string ";" <|>
-    string "="
-    <?> "Punctuator"
-
+    parsedPunctuatorString <-
+        string "[" <|>
+        string "]" <|>
+        string "(" <|>
+        string ")" <|>
+        string "{" <|>
+        string "}" <|>
+        try (string "...") <|>
+        string "." <|>
+        try (string "->") <|>
+        try (string "++") <|>
+        try (string "--") <|>
+        try (string "*=") <|>
+        try (string "/=") <|>
+        try (string "%=") <|>
+        try (string "+=") <|>
+        try (string "-=") <|>
+        try (string "<<=") <|>
+        try (string ">>=") <|>
+        try (string "&=") <|>
+        try (string "^=") <|>
+        try (string "|=") <|>
+        try (string "<=") <|>
+        try (string ">=") <|>
+        try (string "==") <|>
+        try (string "!=") <|>
+        try (string "<:") <|>
+        try (string ":>") <|>
+        try (string "<%") <|>
+        try (string "%>") <|>
+        try (string "%:%:") <|>
+        try (string "%:") <|>
+        try (string "&&") <|>
+        try (string "||") <|>
+        string "&" <|>
+        string "*"<|>
+        string "+" <|>
+        string "-" <|>
+        string "~" <|>
+        string "!" <|>
+        string "/" <|>
+        string "%" <|>
+        try (string "<<") <|>
+        try (string ">>") <|>
+        string "<" <|>
+        string ">" <|>
+        string "^" <|>
+        string "|" <|>
+        string "?" <|>
+        string ":" <|>
+        string ";" <|>
+        string "="
+    return $ Punctuator parsedPunctuatorString
 
