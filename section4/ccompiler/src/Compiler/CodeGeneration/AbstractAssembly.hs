d27fd61db06530afbe5751e09d551ec00cbdbf4f
Peter Bergman <peterbergman8253@gmail.com>
2023-09-24 09:34:10 -0500

    Added new data types for abstract assembly code
    I also added the semantic analysis module to package.yaml in addition to the new abstract assembly module because I forgot to do that in the last commit.

---
+++ section4/ccompiler/src/Compiler/CodeGeneration/AbstractAssembly.hs
@@ -0,0 +1,28 @@
+{-# LANGUAGE DeriveGeneric #-}
+module Compiler.CodeGeneration.AbstractAssembly where
+import Compiler.SymbolTable.SymbolTable
+    (SymbolName)
+import GHC.Generics
+
+newtype AbstractAssemblyFile =
+    AbstractAssemblyFile [AssemblyDefinition]
+    deriving (Show, Generic)
+
+data AssemblyDefinition =
+    AssemblyDefinition SymbolName [AssemblyInstructionOrDirective]
+    deriving (Show, Generic)
+
+data AssemblyInstructionOrDirective =
+    AssemblyInstruction AssemblyInstruction
+    AssemblyDirective AssemblyDirective
+    deriving (Show, Generic)
+
+-- TODO: implement
+data AssemblyInstruction =
+    AssemblyInstructionNotImplementedYet
+    deriving (Show, Generic)
+
+data AssemblyDirective =
+    ReserveBytesDirective Integer
+    deriving (Show, Generic)
+
