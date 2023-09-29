00eadba2c4a3f5a84d325086cd8df60192d4b22d
Peter Bergman <peterbergman8253@gmail.com>
2023-09-29 17:39:37 -0500

    I finished the minimal compiler pipeline.

--- section4/ccompiler/app/Main.hs
+++ section4/ccompiler/app/Main.hs
@@ -12,8 +12,29 @@ import System.Environment
 import Text.Parsec.Prim
 import Text.Parsec.Error
 
-main :: IO ()
-main = putStrLn "C Compiler in development..."
+
+main :: IO Integer
+main = do
+    putStrLn "C Compiler in development, but here goes..."
+    args <- getArgs
+    let inputFileName = args !! 0
+    putStrLn $ "input file name: \"" ++ inputFileName ++ "\""
+    fileContents <- readFile inputFileName
+    putStrLn $ "Below are the file contents:\n\n" ++ fileContents
+    -- compile the file contents
+    let lexedString = lexString fileContents
+    let eitherAstOrError = parse translationUnit inputFileName lexedString
+    let ast = fromRight (error (show eitherAstOrError)) eitherAstOrError
+    let symbolTable = convertAbstractSyntaxTreeToSymbolTable ast
+    let abstractAssemblyFile = generateAbstractAssemblyFromSymbolTable symbolTable
+    let serializedAssembly = toAssembly abstractAssemblyFile
+    -- write the compiled assembly to the output file
+    putStrLn $ "I worked very hard to compile your code. Here is the output assembly:\n\n" ++ serializedAssembly
+    writeFile outputFileName serializedAssembly
+    return 0
+
+outputFileName :: String
+outputFileName = "output.S"
 
 lexedString :: [String]
 lexedString = lexString "int x;\n"
